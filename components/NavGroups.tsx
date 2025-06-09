7a48701adb2ad29563dfff962f9026c8e1157b5f
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2025-06-10 01:23:23 +0200

    Update NavGroups.tsx

--- components/NavGroups.tsx
+++ components/NavGroups.tsx
@@ -33,48 +33,55 @@ export function NavGroups({
     }[];
   }[];
 }) {
+  console.log(groups)
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Groups</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
-          {groups.map((group) => (
-            <Collapsible key={group.name}>
-              <SidebarMenuItem>
-                <TextHover text={group.name} key={group.name}>
-                  <SidebarMenuButton asChild>
-                    <Link href={group.pages[0].url}>
-                      <span>{group.emoji}</span>
-                      <span>{group.name}</span>
-                    </Link>
-                  </SidebarMenuButton>{" "}
-                </TextHover>
-                <CollapsibleTrigger asChild>
-                  <SidebarMenuAction
-                    className="bg-sidebar-accent text-sidebar-accent-foreground left-2 data-[state=open]:rotate-90"
-                    showOnHover
-                  >
-                    <ChevronRight />
-                  </SidebarMenuAction>
-                </CollapsibleTrigger>
+          {groups.length === 0 ? (
+            <span className="p-2 text-xs text-gray-500">
+              There is no Groups for now 😞
+            </span>
+          ) : (
+            groups.map((group) => (
+              <Collapsible key={group.name}>
+                <SidebarMenuItem>
+                  <TextHover text={group.name} key={group.name}>
+                    <SidebarMenuButton asChild>
+                      <Link href={group.pages[0].url}>
+                        <span>{group.emoji}</span>
+                        <span>{group.name}</span>
+                      </Link>
+                    </SidebarMenuButton>{" "}
+                  </TextHover>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuAction
+                      className="bg-sidebar-accent text-sidebar-accent-foreground left-2 data-[state=open]:rotate-90"
+                      showOnHover
+                    >
+                      <ChevronRight />
+                    </SidebarMenuAction>
+                  </CollapsibleTrigger>
 
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {group.pages.map((page) => (
-                      <SidebarMenuSubItem key={page.name}>
-                        <SidebarMenuSubButton asChild>
-                          <Link href={page.url}>
-                            <span>{page.emoji}</span>
-                            <span>{page.name}</span>
-                          </Link>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </SidebarMenuItem>
-            </Collapsible>
-          ))}
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {group.pages.map((page) => (
+                        <SidebarMenuSubItem key={page.name}>
+                          <SidebarMenuSubButton asChild>
+                            <Link href={page.url}>
+                              <span>{page.emoji}</span>
+                              <span>{page.name}</span>
+                            </Link>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      ))}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            ))
+          )}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
